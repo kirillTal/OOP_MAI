c79f6e94440e479668f2b9c241410105e71521fc
kirill_Tal <kirill.tal2005@gmail.com>
2024-10-05 12:47:00 +0300

    1

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kirill/lab1
-# Build directory: /home/kirill/lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1 "tests")
-set_tests_properties(TestLab1 PROPERTIES  _BACKTRACE_TRIPLES "/home/kirill/lab1/CMakeLists.txt;26;add_test;/home/kirill/lab1/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
