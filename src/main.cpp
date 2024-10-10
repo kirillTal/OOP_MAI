a47a70ef14546124b0d72e2d1edfbbb4eb2401cc
kirill_Tal <kirill.tal2005@gmail.com>
2024-10-10 23:25:23 +0300

    2

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/solution.h"
+int main() {    
+    Four num1("12");
+    Four num2("02");     
+
+    std::cout << "Number 1: " << num1 << "\n";    
+    std::cout << "Number 2: " << num2 << "\n";
+    return 0;
+    }
